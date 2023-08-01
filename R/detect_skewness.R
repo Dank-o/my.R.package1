e1ba1d387916e93a313911bcef167f17fe3fbd12
Dank-o <h.madis@gmail.com>
2023-08-01 22:03:23 +0100

    description corrections

--- R/detect_skewness.R
+++ R/detect_skewness.R
@@ -1,3 +1,11 @@
+#' Detect Skewness
+#'
+#' @param A vector or list
+#'
+#' @return A character string
+#' @export
+#'
+#' @examples detect_skewness(c(1, 2, 3, -5, 5, 5, 6, 8, 10))
 detect_skewness <- function(sample) {
   mean_val <- mean(sample)
   median_val <- median(sample)
